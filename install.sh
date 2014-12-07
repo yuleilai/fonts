e26319db1f1432f59ff119f9d009710a44875b46
yuleilai <yuleilai@icloud.com>
2014-12-07 15:02:20 +0800

    Delete YaHeiConsolas.ttf

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ echo "Start install"
 sudo mkdir -p /usr/share/fonts/truetype/custom
 
 echo "Downloading fonts"
-wget -c -l1 -A.ttf https://raw.githubusercontent.com/yuleilai/fonts/master/
+wget -c https://raw.githubusercontent.com/yuleilai/fonts/master/DejaVuSansMono.ttf 
+wget -c https://raw.githubusercontent.com/yuleilai/fonts/master/Monaco.ttf
 
 echo "Installing fonts"
 sudo mv *.ttf /usr/share/fonts/truetype/custom/
